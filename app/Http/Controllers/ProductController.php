4b9ec259ad76a67d40a1e131d78c600847efa210
hiphop200199 <hiphop200199@gmail.com>
2024-10-05 12:42:00 +0800

    write top 10 popular products controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -71,6 +72,17 @@ class ProductController extends Controller
     public function getMonthlyNewProducts()
     {
         $products = Product::whereBetween('launched_date', [date('2024-10-01'), date('Y-m-d')])->paginate(10);
+        return $products;
+    }
+    public function getPopularProducts()
+    {
+        $popular_products_list = DB::table('order_details')->select(['product_id'])->orderBy(DB::raw('count(product_id)','DESC'))->take(10)->get();
+        $products = Product::select(['id','name','image_source','price'])
+        ->joinSub($popular_products_list,'popular',function($join)
+        {
+            $join->on('products.id','=','popular.product_id');
+        }
+        )->get();
         return $products;
     }
     public function getProduct(Request $request)
